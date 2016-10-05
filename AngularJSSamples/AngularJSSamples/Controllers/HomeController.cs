c6406b2bbb2bff8b99107e7d80747272e529e83c
supershowwei <supershowwei@gmail.com>
2016-10-05 22:32:16 +0800

    complete

---
+++ AngularJSSamples/AngularJSSamples/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Web.Mvc;
+
+namespace AngularJSSamples.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
